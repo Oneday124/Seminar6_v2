31529aeaa64e88a9c939f6f3db2c24933904075f
Nikolay <Onedaydejavu124@mail.ru>
2022-11-23 22:22:58 +0700

    Напишите программу, которая перевернет одномерный массив (последний будет первым, а первый последним)

--- EX001/Program.cs
+++ EX001/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая перевернет одномерный массив (последний будет первым, а первый последним)
+// Реализация 1 Без создания нового массива
+
+void RevertArray(int[] array)
+{
+    for(int i = 0; i < array.Length / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length -1 - i];
+        array[array.Length -1 - i] = temp;
+    }
+}
+
+ void PrintArray(int[] array)
+ {
+    System.Console.WriteLine(string.Join(", ",array));
+ }
+
+int[] array = new int[]{ 1, 2, 3, 4, 5 };
+
+RevertArray(array);
+PrintArray(array);

ce9787d1287e1464bd9c0a1590428f6d740ac1c6
Nikolay <Onedaydejavu124@mail.ru>
2022-11-24 00:43:30 +0700

    Напишите программу, которая будет преобразовывать десятичное число в двоичное. Через массив

--- EX003/Program.cs
+++ EX003/Program.cs
@@ -1,42 +1,22 @@
-﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное. Через массивы
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное. Через массив
 
-int LengthArray (int num)
+int[] GetBinaryArray(int num)
 {
-    int i = 0;
-    while (num > 0)
-    {
-        int ost = num % 2;
-        num = num / 2;
-        i++;
-    }
-    return i;
-}
-void FillArray(int[] array, int num)
-{
+    int[] array = new int[(int)Math.Log2(num) + 1 ];
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = num % 2;
+        array[^(i+1)] = num % 2;
         num = num / 2;
     }
+    return array;
 }
-void RevertArray(int[] array)
-{
-    for(int i = 0; i < array.Length / 2; i++)
-    {
-        int temp = array[i];
-        array[i] = array[array.Length -1 - i];
-        array[array.Length -1 - i] = temp;
-    }
-}
+
 void PrintArray(int[] array)
 {
-    Console.WriteLine(string.Join("",array));
+    System.Console.WriteLine(string.Join("",array));
 }
 
 System.Console.WriteLine("Введите число: ");
 int num = int.Parse(Console.ReadLine() !);
 
-int[] array = new int[LengthArray(num)];
-FillArray(array, num);
-RevertArray(array);
-PrintArray(array);
+PrintArray(GetBinaryArray(num));
